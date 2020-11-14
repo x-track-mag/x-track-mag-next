b60f6b22c2372f8c66367da52b8c7ae0d6bae6ea
zipang <christophe.Desguez@gmail.com>
2020-11-14 02:34:53 +0100

    Scripts

---
+++ src/scripts/serializePosts.js
@@ -0,0 +1,34 @@
+import fs from "fs-extra";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import Env from "../lib/utils/Env.js";
+import { getPosts } from "../lib/server/PrismicSDK.js";
+
+// REBUILD THE COMMON JS ENV VARIABLES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const transformPrismicData = (post) => {};
+
+const serializePosts = async () => {
+	Env.loadEnv();
+	const posts = await getPosts();
+	const contentDir = path.join(__dirname, "../../content");
+	await fs.ensureDir(contentDir);
+	const postsDir = path.join(contentDir, "posts");
+	await fs.ensureDir(postsDir);
+
+	// serialize all the posts
+	const serialized = JSON.stringify(posts, null, "\t");
+	await fs.writeFile(path.join(contentDir, "posts.json"), serialized);
+
+	// Now serialize each post individually
+	posts.forEach(async (post) => {
+		const postFileName = path.join(postsDir, `${post.uid}.json`);
+		await fs.writeFile(postFileName, JSON.stringify(post, null, "\t"));
+	});
+
+	console.log(`Files have been written to disk`);
+};
+
+serializePosts();
