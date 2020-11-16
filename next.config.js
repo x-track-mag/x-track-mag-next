6f597372038b6d1f01eec241fd1cc4dcfdb4f95f
zipang <christophe.desguez@gmail.com>
2020-11-16 17:28:40 +0100

    New configuration for next/image and @content alias

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const { resolve } = require("path");
 
 module.exports = {
 	reactStrictMode: true,
+	images: {
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+		domains: ["images.prismic.io"],
+		path: "/_next/image",
+		loader: "default"
+	},
 	webpack: (config, options) => {
 		config.module.rules.push({
 			test: /\.md$/,
@@ -21,7 +28,8 @@ module.exports = {
 			"@lib": resolve(__dirname, "src/lib/"),
 			"@components": resolve(__dirname, "src/components/"),
 			"@forms": resolve(__dirname, "src/components/forms/"),
-			"@api": resolve(__dirname, "src/pages/api")
+			"@api": resolve(__dirname, "src/pages/api"),
+			"@content": resolve(__dirname, "content")
 		};
 
 		// Fixes npm packages that depend on `fs` module
