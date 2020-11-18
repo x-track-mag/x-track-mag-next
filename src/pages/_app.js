13bf952b8430117fbc26624acc4fc8097902a889
zipang <christophe.desguez@gmail.com>
2020-11-18 14:42:58 +0100

    Added the ViewportSizeProvider to every pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import "../styles/theme.scss";
+import global from "../styles/global.scss";
+import { ThemeProvider } from "theme-ui";
+import ViewportSizeProvider from "@components/ViewportSizeProvider.js";
+import theme from "../styles/theme-w-typography.js";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
+	<ThemeProvider theme={theme}>
+		<ViewportSizeProvider>
+			<Component {...pageProps} />
+		</ViewportSizeProvider>
+	</ThemeProvider>
+);
 
 export default MyApp;
