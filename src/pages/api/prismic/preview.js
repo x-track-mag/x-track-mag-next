054e928ee482a2920fa009fe2f28dda508679d75
zipang <christophe.desguez@gmail.com>
2020-11-20 11:27:06 +0100

    Corrected all the compilations errors preventing the project to be build

--- src/pages/api/prismic/preview.js
+++ src/pages/api/prismic/preview.js
@@ -1,41 +1,41 @@
-import { PrismicClient } from '../../lib/api'
+import { getInstance } from "@lib/server/PrismicSDK";
 
 function linkResolver(doc) {
-  // Pretty URLs for known types
-  if (doc.type === 'post') {
-    return `/posts/${doc.uid}`
-  }
+	// Pretty URLs for known types
+	if (doc.type === "post") {
+		return `/posts/${doc.uid}`;
+	}
 
-  // Fallback for other types, in case new custom types get created
-  return `/${doc.uid}`
+	// Fallback for other types, in case new custom types get created
+	return `/${doc.uid}`;
 }
 
 export default async function preview(req, res) {
-  const { token: ref, documentId } = req.query
+	const { token: ref, documentId } = req.query;
+	const client = getInstance();
 
-  // Check the token parameter against the Prismic SDK
-  const url = await PrismicClient.getPreviewResolver(ref, documentId).resolve(
-    linkResolver,
-    '/'
-  )
+	// Check the token parameter against the Prismic SDK
+	const url = await client
+		.getPreviewResolver(ref, documentId)
+		.resolve(linkResolver, "/");
 
-  if (!url) {
-    return res.status(401).json({ message: 'Invalid token' })
-  }
+	if (!url) {
+		return res.status(401).json({ message: "Invalid token" });
+	}
 
-  // Enable Preview Mode by setting the cookies
-  res.setPreviewData({
-    ref, // pass the ref to pages so that they can fetch the draft ref
-  })
+	// Enable Preview Mode by setting the cookies
+	res.setPreviewData({
+		ref // pass the ref to pages so that they can fetch the draft ref
+	});
 
-  // Redirect the user to the share endpoint from same origin. This is
-  // necessary due to a Chrome bug:
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=696204
-  res.write(
-    `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${url}" />
+	// Redirect the user to the share endpoint from same origin. This is
+	// necessary due to a Chrome bug:
+	// https://bugs.chromium.org/p/chromium/issues/detail?id=696204
+	res.write(
+		`<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${url}" />
     <script>window.location.href = '${url}'</script>
     </head>`
-  )
+	);
 
-  res.end()
+	res.end();
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -24,14 +24,4 @@ export const getStaticProps = async ({ params, preview }) => {
 	};
 };
 
-/**
- * We use the serialized data from the static JSON file @content/paths.json
- */
-export const getStaticPaths = () => ({
-	paths: paths.map((uid) => ({
-		params: { uid }
-	})),
-	fallback: false
-});
-
 export default HomePage;
