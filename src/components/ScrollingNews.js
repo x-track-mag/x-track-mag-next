02ac53f40ba04a15b177cb3caf53db60334fb14d
zipang <christophe.desguez@gmail.com>
2020-11-20 01:18:58 +0100

    Fixed header with scrolling news

---
+++ src/components/ScrollingNews.js
@@ -0,0 +1,25 @@
+import Ticker from "react-ticker";
+
+import { Message } from "@components/base/Typography";
+
+const ScrollingNews = ({ messages = [], delay = 500 }) => {
+	if (!messages.length) return;
+	const message = messages.join("   -   ");
+
+	return (
+		<div
+			id="scrolling-news"
+			style={{
+				position: "fixed",
+				width: "100%",
+				overflow: "hidden",
+				top: "5em",
+				height: "4em"
+			}}
+		>
+			<Ticker>{() => <Message>{message}</Message>}</Ticker>
+		</div>
+	);
+};
+
+export default ScrollingNews;
