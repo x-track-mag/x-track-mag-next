1ba1208b67bdc6727f33dc1f81380d9164a58df8
zipang <christophe.desguez@gmail.com>
2020-11-20 01:24:19 +0100

    Added style for the scrolling Message

--- src/components/base/Typography.js
+++ src/components/base/Typography.js
@@ -12,6 +12,19 @@ export const Title = ({ children }) => (
 	</chakra.h2>
 );
 
+export const Message = ({ children }) => (
+	<chakra.span
+		fontFamily="Arachne"
+		fontSize="2.8rem"
+		fontWeight="600"
+		lineHeight="4rem"
+		textColor="brand.green"
+		m="0"
+	>
+		{children}
+	</chakra.span>
+);
+
 export const Subtitle = ({ children }) => (
 	<chakra.h3 fontFamily="Verdana" fontSize="1.5rem">
 		{children}
