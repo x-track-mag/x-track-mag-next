6bc4d074035806e22599ff7da9499138405e18fc
zipang <christophe.desguez@gmail.com>
2020-11-21 18:36:57 +0100

    Our Container

--- src/components/base/Container.js
+++ src/components/base/Container.js
@@ -1,7 +1,35 @@
 import { chakra } from "@chakra-ui/react";
 
-export default Container = ({ children }) => (
-	<chakra.div m="0 auto" p="2rem 0" width="100%" maxWidth={{ sm: "90%", md: "80ch" }}>
+const ContainerStyles = {
+	baseStyle: {
+		width: "100%",
+		maxWidth: ["95%", "80%", "80ch"],
+		m: "0 auto",
+		p: "2rem 0"
+	},
+	variants: {
+		hero: {
+			maxWidth: ["80%"]
+		},
+		fluid: {
+			maxWidth: "100%",
+			p: "2rem"
+		}
+	},
+	defaultProps: {}
+};
+
+const Container = ({ children, ...moreStyles }) => (
+	<chakra.div
+		className="container"
+		m="0 auto"
+		p="2rem 0"
+		width="100%"
+		maxWidth={["95%", "80%", "60ch"]}
+		{...moreStyles}
+	>
 		{children}
 	</chakra.div>
 );
+
+export default Container;
