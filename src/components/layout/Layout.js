5756e4b6f911618cc52b84456fa246dafb780d92
zipang <christophe.desguez@gmail.com>
2020-11-21 18:36:39 +0100

    Try a fade in fade out on pages transitions

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,6 +2,8 @@
 
 import Header from "./Header";
 import Footer from "./Footer";
+import { useRouter } from "next/router";
+import { motion, AnimatePresence } from "framer-motion";
 
 const layoutStyle = {
 	display: "flex",
@@ -19,9 +21,19 @@ const contentStyle = {
 const Layout = ({ children }) => (
 	<div className="layout" {...layoutStyle}>
 		<Header />
-		<main id="main-content" {...contentStyle}>
-			{children}
-		</main>
+		<AnimatePresence>
+			<motion.main
+				key={useRouter().route}
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				duration={10}
+				exit={{ opacity: 0 }}
+				id="main-content"
+				{...contentStyle}
+			>
+				{children}
+			</motion.main>
+		</AnimatePresence>
 		<Footer />
 	</div>
 );
