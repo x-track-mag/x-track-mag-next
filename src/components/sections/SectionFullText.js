9266831888c371c09e81283747c6e892312ed5fd
zipang <christophe.desguez@gmail.com>
2020-11-21 13:32:27 +0100

    Use chakra Box to allow passing more props

--- src/components/sections/SectionFullText.js
+++ src/components/sections/SectionFullText.js
@@ -1,14 +1,13 @@
 import { RichText } from "prismic-reactjs";
 import { Box, Container } from "@chakra-ui/react";
-import clsx from "clsx";
 
 /**
  * A centered/responsive single column of rich text
  * @param {JSXElement} props
  * @param {RichText} text
  */
-const SectionFullText = ({ text }) => (
-	<Box as="section" className="section-full-text">
+const SectionFullText = ({ text, ...props }) => (
+	<Box as="section" className="section-full-text" {...props}>
 		<Container padding="3rem 0">
 			<RichText render={text} />
 		</Container>
