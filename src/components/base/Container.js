c5ab09bf28eae816cc7c1256a67c6b3731a83e73
zipang <christophe.desguez@gmail.com>
2020-11-22 17:02:41 +0100

    Removed unused ContainerStyles until we figure out how to use it

--- src/components/base/Container.js
+++ src/components/base/Container.js
@@ -1,31 +1,12 @@
 import { chakra } from "@chakra-ui/react";
 
-const ContainerStyles = {
-	baseStyle: {
-		width: "100%",
-		maxWidth: ["95%", "80%", "80ch"],
-		m: "0 auto",
-		p: "2rem 0"
-	},
-	variants: {
-		hero: {
-			maxWidth: ["80%"]
-		},
-		fluid: {
-			maxWidth: "100%",
-			p: "2rem"
-		}
-	},
-	defaultProps: {}
-};
-
 const Container = ({ children, ...moreStyles }) => (
 	<chakra.div
 		className="container"
+		width="100%"
+		maxWidth={["95%", "80%", "60ch", "70ch"]}
 		m="0 auto"
 		p="2rem 0"
-		width="100%"
-		maxWidth={["95%", "80%", "60ch"]}
 		{...moreStyles}
 	>
 		{children}
