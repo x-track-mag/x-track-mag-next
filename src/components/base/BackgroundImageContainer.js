2472c2591e03534d63c37423d914b36b485083cf
zipang <christophe.desguez@gmail.com>
2020-11-24 23:22:12 +0100

    Added JSDoc + allow to override styles

--- src/components/base/BackgroundImageContainer.js
+++ src/components/base/BackgroundImageContainer.js
@@ -2,10 +2,22 @@ import Image from "next/image";
 import { Box } from "@chakra-ui/react";
 
 /**
- *
+ * Use a Next.JS responsive Image component to fill and cover
+ * exactly like a background image would do
+ * Note : The BackgroundImageContainer must be used inside
+ * a parent container with intrisic dimensions and positioning enabled
+ * (eg. : position relative)
  * @param {JSXElement} props
+ * @param {ImageDescr} props.image
+ * @param {CSSDimension} [props.width="100%"]
+ * @param {CSSDimension} [props.height="100%"]
  */
-const BackgroundImageContainer = ({ image, width = "100%", height = "100%" }) => {
+const BackgroundImageContainer = ({
+	image,
+	width = "100%",
+	height = "100%",
+	...moreStyle
+}) => {
 	if (!image) return null;
 
 	// Calculate the width and height and position to fill the container
@@ -17,6 +29,7 @@ const BackgroundImageContainer = ({ image, width = "100%", height = "100%" }) =>
 			position="absolute"
 			zIndex="-1"
 			overflow="hidden"
+			{...moreStyle}
 		>
 			<Image
 				className="background-image"
