4d96e361a4f1417276103e8dc6c6140e567ed622
zipang <christophe.desguez@gmail.com>
2020-11-29 14:47:14 +0100

    Added Cross Origin Requests Support (CORS) to allow Prismic to call our update Hook

--- src/pages/api/prismic/update.js
+++ src/pages/api/prismic/update.js
@@ -1,10 +1,23 @@
 import { extractData } from "../../../scripts/extract-data.js";
+import Cors from "cors";
+import initMiddleware from "@lib/server/init-middleware.js";
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+	// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+	Cors({
+		// Only allow requests with GET, POST and OPTIONS
+		methods: ["POST", "OPTIONS"]
+	})
+);
 
 /**
  * Extract latest data from Prismic and push it to the git repo to trigger a new build
  */
 const update = async (req, resp) => {
 	try {
+		// Run cors
+		await cors(req, resp);
 		console.dir(`Received Prismic update hook :`, req.body);
 		const status = await extractData();
 		resp.json({ success: true, message: "New content is being redeployed" });
