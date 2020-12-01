f167f3e6921d66e05d642e80b070e24b913d9e42
zipang <christophe.desguez@gmail.com>
2020-12-01 20:38:47 +0100

    Fix : take whatever image there is as the video launcher

--- src/components/sections/SectionVideoLauncher.js
+++ src/components/sections/SectionVideoLauncher.js
@@ -11,50 +11,52 @@ import SvgPlayerIcon from "@components/icons/SvgPlayerIcon.js";
  * @param {String} props.text the rich text content to describe the video
  * @param {String} props.link Youtube or Vimeo media link in the format that Prismic
  */
-const SectionVideoLauncher = ({ article, text, link, ...props }) => (
-	<Box as="section" className="section-video-launcher" {...props}>
-		<Container
-			as={Grid}
-			fluid={true}
-			templateColumns={{ base: "100%", lg: "40% auto" }}
-			gap={{ base: "0", lg: "2rem" }}
-		>
-			<GridItem padding="2rem">
-				<Title>{article.title}</Title>
-				<Subtitle>{article.subtitle}</Subtitle>
-				<Box pt="1rem">
-					<RichText>{text}</RichText>
-				</Box>
-			</GridItem>
-			<GridItem
-				padding="2rem"
-				as={Flex}
-				flexDirection="column"
-				alignItems="center"
-				justifyContent="center"
+const SectionVideoLauncher = ({ article, text, image, link, ...props }) => {
+	image = image || article.image;
+	return (
+		<Box as="section" className="section-video-launcher" {...props}>
+			<Container
+				as={Grid}
+				fluid={true}
+				templateColumns={{ base: "100%", lg: "40% auto" }}
+				gap={{ base: "0", lg: "2rem" }}
 			>
-				<AspectRatio ratio={16 / 9} width="100%">
-					<ReactPlayer
-						url={link.embed_url}
-						light={article.image ? article.image.url : false} // display the image as vignette
-						width="100%"
-						height="100%"
-						playIcon={<SvgPlayerIcon size="4rem" />}
-						config={{
-							youtube: {
-								playerVars: {
-									autoplay: 1,
-									loop: 1,
-									modestbranding: 1,
-									hl: "fr"
+				<GridItem padding="2rem">
+					<Title>{article.title}</Title>
+					<Subtitle>{article.subtitle}</Subtitle>
+					<Box pt="1rem">
+						<RichText>{text}</RichText>
+					</Box>
+				</GridItem>
+				<GridItem
+					padding="2rem"
+					as={Flex}
+					flexDirection="column"
+					alignItems="center"
+					justifyContent="center"
+				>
+					<AspectRatio ratio={16 / 9} width="100%">
+						<ReactPlayer
+							url={link.embed_url}
+							light={image ? image.url : false} // display the image as vignette
+							width="100%"
+							height="100%"
+							playIcon={<SvgPlayerIcon size="4rem" />}
+							config={{
+								youtube: {
+									playerVars: {
+										autoplay: 1,
+										loop: 1,
+										modestbranding: 1,
+										hl: "fr"
+									}
 								}
-							}
-						}}
-					/>
-				</AspectRatio>
-			</GridItem>
-		</Container>
-	</Box>
-);
-
+							}}
+						/>
+					</AspectRatio>
+				</GridItem>
+			</Container>
+		</Box>
+	);
+};
 export default SectionVideoLauncher;
