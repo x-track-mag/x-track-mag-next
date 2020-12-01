9f5b72ba93ea1ca1eb12c26ea1fd42f47f2216b8
zipang <christophe.desguez@gmail.com>
2020-12-01 18:19:57 +0100

    Hack the viewbox to align the arrow on the right edge

--- src/components/icons/SvgArrowReturn.js
+++ src/components/icons/SvgArrowReturn.js
@@ -5,7 +5,7 @@
  * @param {String} [props.size="1rem"] a valid CSS size unit
  */
 const SvgArrowReturn = ({ color = "#000", size = "1rem" }) => (
-	<svg viewBox="0 0 32 32" width={size} height={size}>
+	<svg viewBox="-7 0 32 32" width={size} height={size}>
 		<g fill="none" stroke={color} strokeLinejoin="round">
 			<path d="M24 6v13H8" />
 			<path strokeLinecap="round" d="M15 14l-7 5 7 5" />
