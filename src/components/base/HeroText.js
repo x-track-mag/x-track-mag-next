8b0e2ac084f23ac633c5dfbd14aef39cbdcde9a1
zipang <christophe.desguez@gmail.com>
2020-12-04 13:46:24 +0100

    Second try

--- src/components/base/HeroText.js
+++ src/components/base/HeroText.js
@@ -1,24 +1,26 @@
 import { Container } from "@chakra-ui/react";
 import { Title, Subtitle } from "@components/base/Typography";
-// import { useRef } from "react";
-// import { useApexPosition } from "@components/hooks/useApexPosition";
+import { useRef } from "react";
+import { useApexPosition } from "@components/hooks/useApexPosition";
 
 /**
  * A centered block of text suited for Hero sections
  */
 const HeroText = ({ title, subtitle, ...moreStyles }) => {
-	// const ref = useRef();
-	// const [inView, apexPosition] = useApexPosition(ref);
+	const ref = useRef();
+	const [inView, apexPosition] = useApexPosition(ref);
+	if (inView && typeof window !== "undefined") {
+		moreStyles = {
+			...moreStyles,
+			animate: {
+				x: 100 - 100 * apexPosition, // Starts 100px from the right
+				opacity: apexPosition //
+			}
+		};
+	}
 	return (
 		<Container ref={ref} centerContent p="2rem">
-			{title && title !== "--blank--" && (
-				<Title
-					{...moreStyles}
-					// animate={{ x: 100 - 100 * apexPosition, opacity: apexPosition }}
-				>
-					{title}
-				</Title>
-			)}
+			{title && title !== "--blank--" && <Title {...moreStyles}>{title}</Title>}
 			{subtitle && subtitle !== "--blank--" && (
 				<Subtitle {...moreStyles}>{subtitle}</Subtitle>
 			)}
