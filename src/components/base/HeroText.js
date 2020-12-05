0a27057fb4a49cc7675013cbac80321fb320ae58
zipang <christophe.desguez@gmail.com>
2020-12-05 17:05:30 +0100

    Changed the easing and even duration for exit

--- src/components/base/HeroText.js
+++ src/components/base/HeroText.js
@@ -2,6 +2,7 @@ import { Container } from "@chakra-ui/react";
 import { Title, Subtitle } from "@components/base/Typography";
 import { useRef } from "react";
 import { useApexPosition } from "@components/hooks/useApexPosition";
+import useUniversalEffect from "@components/hooks/useUniversalEffect";
 
 /**
  * A centered block of text suited for Hero sections
@@ -9,23 +10,33 @@ import { useApexPosition } from "@components/hooks/useApexPosition";
 const HeroText = ({ title, subtitle, ...moreStyles }) => {
 	const ref = useRef();
 	const [inView, apexPosition] = useApexPosition(ref);
-	if (typeof window !== "undefined") {
-		if (inView) {
-			moreStyles = {
-				...moreStyles,
-				animate: {
-					x: 100 - 100 * apexPosition,
-					opacity: inView ? apexPosition : 0
-				}
-			};
-		} else {
-			moreStyles = {
-				...moreStyles,
-				opacity: 0, // Starts at opacity 0
-				x: 100 // Starts 100px from the right
-			};
-		}
+
+	if (inView) {
+		moreStyles = {
+			...moreStyles,
+			transition: {
+				ease: "easeOut"
+			},
+			animate: {
+				x: 100 - 100 * apexPosition,
+				opacity: apexPosition
+			}
+		};
+	} else {
+		// Don't allow to flicker when entering the viewport
+		moreStyles = {
+			...moreStyles,
+			transition: {
+				ease: "easeOut",
+				duration: 0
+			},
+			animate: {
+				x: 100, // Starts 100px from the right
+				opacity: 0 // Starts at opacity 0
+			}
+		};
 	}
+
 	return (
 		<Container ref={ref} centerContent p="2rem">
 			{title && title !== "--blank--" && <Title {...moreStyles}>{title}</Title>}
