e9bde5d83b4e95fec04a5a0b43c4d812e3126184
zipang <christophe.desguez@gmail.com>
2020-12-06 22:13:47 +0100

    Pass the props for the SEOMeta tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,12 +17,14 @@ export const HomePage = ({ scrolling_news, sections }) => {
  * else, read the serialized JSON file that we extracted inside the @content dir
  */
 export const getStaticPropsFor = (tagName) => async ({ params, preview }) => {
-	let { scrolling_news, pinned_posts, sections } = await import(`@content/home.json`);
+	let { scrolling_news, pinned_posts, sections, ...pageProps } = await import(
+		`@content/home.json`
+	);
 	if (tagName !== "*") {
 		sections = sections.filter((post) => post.tags.includes(tagName));
 	}
 	return {
-		props: { scrolling_news, pinned_posts, sections }
+		props: { scrolling_news, pinned_posts, sections, ...pageProps }
 	};
 };
 
