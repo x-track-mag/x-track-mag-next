ddb96fe05a4d77bf43a211775b4a2134465fa33a
zipang <christophe.desguez@gmail.com>
2020-12-06 19:09:30 +0100

    Debugging : Display the error message when it fails

--- src/components/base/HeroText.js
+++ src/components/base/HeroText.js
@@ -8,43 +8,54 @@ import useUniversalEffect from "@components/hooks/useUniversalEffect";
  * A centered block of text suited for Hero sections
  */
 const HeroText = ({ title, subtitle, ...moreStyles }) => {
-	const ref = useRef();
-	const [inView, apexPosition] = useApexPosition(ref);
+	try {
+		const ref = useRef();
+		const [inView, apexPosition] = useApexPosition(ref);
 
-	if (inView) {
-		moreStyles = {
-			...moreStyles,
-			transition: {
-				ease: "easeOut"
-			},
-			animate: {
-				x: 100 - 100 * apexPosition,
-				opacity: apexPosition
-			}
-		};
-	} else {
-		// Don't allow to flicker when entering the viewport
-		moreStyles = {
-			...moreStyles,
-			transition: {
-				ease: "easeOut",
-				duration: 0
-			},
-			animate: {
-				x: 100, // Starts 100px from the right
-				opacity: 0 // Starts at opacity 0
-			}
-		};
+		if (inView) {
+			moreStyles = {
+				...moreStyles,
+				transition: {
+					ease: "easeOut"
+				},
+				animate: {
+					x: 100 - 100 * apexPosition,
+					opacity: apexPosition
+				}
+			};
+		} else {
+			// Don't allow to flicker when entering the viewport
+			moreStyles = {
+				...moreStyles,
+				transition: {
+					ease: "easeOut",
+					duration: 0
+				},
+				animate: {
+					x: 100, // Starts 100px from the right
+					opacity: 0 // Starts at opacity 0
+				}
+			};
+		}
+
+		return (
+			<Container ref={ref} centerContent p="2rem">
+				{title && title !== "--blank--" && <Title {...moreStyles}>{title}</Title>}
+				{subtitle && subtitle !== "--blank--" && (
+					<Subtitle {...moreStyles}>{subtitle}</Subtitle>
+				)}
+			</Container>
+		);
+	} catch (err) {
+		return (
+			<pre>
+				<code>
+					{err.message}
+					{err.stack}
+				</code>
+			</pre>
+		);
 	}
-
-	return (
-		<Container ref={ref} centerContent p="2rem">
-			{title && title !== "--blank--" && <Title {...moreStyles}>{title}</Title>}
-			{subtitle && subtitle !== "--blank--" && (
-				<Subtitle {...moreStyles}>{subtitle}</Subtitle>
-			)}
-		</Container>
-	);
 };
 
 export default HeroText;
