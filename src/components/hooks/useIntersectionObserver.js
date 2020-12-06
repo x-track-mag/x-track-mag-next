bb762eac462e1f157a28ff1420f5468724b225d0
zipang <christophe.desguez@gmail.com>
2020-12-06 15:29:06 +0100

    JSDoc

--- src/components/hooks/useIntersectionObserver.js
+++ src/components/hooks/useIntersectionObserver.js
@@ -1,5 +1,15 @@
 import { useEffect, useState } from "react";
 
+/**
+ * @typedf InterSectionObserverOptions
+ * @param {String} rootMargin CSS dimension around the root element top trigger the isIntersecting event
+ */
+
+/**
+ * Hook to use the IntersectionObserver API on the provided element
+ * @param {*} ref Reference to the element to observe
+ * @param {InterSectionObserverOptions} options
+ */
 const useIntersectionObserver = (ref, options) => {
 	const [intersectionObserverEntry, setIntersectionObserverEntry] = useState(null);
 
