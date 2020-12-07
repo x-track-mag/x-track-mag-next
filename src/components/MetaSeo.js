66c4a9632809d84ba58904f5fccfce3fc7d946f7
zipang <christophe.desguez@gmail.com>
2020-12-07 15:51:40 +0100

    SEO metadata and favicons

--- src/components/MetaSeo.js
+++ src/components/MetaSeo.js
@@ -1,45 +1,74 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import path from "path";
 
 const MetaSEO = ({
 	keywords,
 	tags = [],
 	title = "ACCUEIL",
+	baseUrl = "https://x-track-mag.fr",
+	publication_date,
 	description = "",
 	author = "X-TRACK MAG",
 	lang = "fr",
-	sections,
 	image
 }) => {
 	title = `X-TRACK MAG - ${title}`;
 	const router = useRouter();
-	const canonicalUrl = router.pathname;
+	const canonicalUrl = path.join(baseUrl, router.asPath);
 	const isArticle = tags.find((t) => t === "article");
 	return (
 		<Head>
 			<title>{title}</title>
+			<link rel="canonical" href={canonicalUrl} />
+
+			<meta name="robots" key="robots" content="index,follow" />
+			<meta name="application-name" content="X-TRACK MAG"></meta>
 			<meta name="description" content={description} />
 			<meta name="keywords" content={keywords || tags} />
 			<meta name="lang" content={lang} />
 
-			<meta property="og:type" content="article" />
-			<meta name="og:title" property="og:title" content={title} />
-			<meta name="og:description" property="og:description" content={description} />
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={description} />
 			<meta property="og:site_name" content="X-TRACK MAG" />
 			<meta property="og:url" content={canonicalUrl} />
+			{image && <meta property="og:image" content={image.url} />}
+
+			<meta property="og:type" content={isArticle ? "article" : "website"} />
+			{isArticle && (
+				<meta property="article:modified_time" content={publication_date} />
+			)}
+			{isArticle && author && (
+				<meta key="article:author" property="article:author" content={author} />
+			)}
 
 			<meta name="twitter:card" content="summary" />
-			<meta name="twitter:title" content={title} />
-			<meta name="twitter:description" content={description} />
-			<meta name="twitter:site" content="X-TRACK MAG" />
+			<meta name="twitter:site" content="@x-track-mag" />
 			{author && <meta name="twitter:creator" content={author} />}
 
-			<link rel="apple-touch-icon" href="/img/x-track-mag.png" />
-
-			{image && <meta property="og:image" content={image.url} />}
-			{image && <meta name="twitter:image" content={image.url} />}
-
-			<link rel="canonical" href={canonicalUrl} />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/img/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/img/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/img/favicon-16x16.png"
+			/>
+			<link rel="manifest" href="/img/site.webmanifest" />
+			<link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5" />
+			<link rel="shortcut icon" href="/img/favicon.ico" />
+			<meta name="msapplication-TileColor" content="#ffc40d" />
+			<meta name="msapplication-config" content="/img/browserconfig.xml" />
+			<meta name="theme-color" content="#ffffff" />
 		</Head>
 	);
 };
