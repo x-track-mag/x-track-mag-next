94bdf6cd911210f7e0b4ec984b58f6e28532fa91
zipang <christophe.desguez@gmail.com>
2020-12-08 14:45:32 +0100

    makeMotionBox() with default styling and the ability to forward ref

--- src/components/base/MotionBox.js
+++ src/components/base/MotionBox.js
@@ -6,12 +6,22 @@ import { Box } from "@chakra-ui/react";
  * This will allow us to animate all this typographic elements
  * with framer-motion properties
  * @see https://github.com/chakra-ui/chakra-ui/issues/2538 for the quirks
+ * why framer props are not passed to child component
  */
 // const MotionBox = motion.custom(Box);
 const MotionBox = motion.custom(
-	forwardRef(({ whileHover, animate, transition, ...rest }, ref) => (
+	forwardRef(({ whileHover, animate, transition, variants, initial, ...rest }, ref) => (
 		<Box ref={ref} {...rest} />
 	))
 );
+
+export const makeMotionBox = ({ ...defaultProps }) =>
+	motion.custom(
+		forwardRef(
+			({ whileHover, animate, transition, variants, initial, ...rest }, ref) => (
+				<Box ref={ref} {...defaultProps} {...rest} />
+			)
+		)
+	);
 
 export default MotionBox;
