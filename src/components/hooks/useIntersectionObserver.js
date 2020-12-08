3e1778e67b1c956b456a86e19ee17c5acd424c06
zipang <christophe.desguez@gmail.com>
2020-12-08 09:00:35 +0100

    More documentation with links to the IntersectionObserver API

--- src/components/hooks/useIntersectionObserver.js
+++ src/components/hooks/useIntersectionObserver.js
@@ -1,16 +1,29 @@
 import { useEffect, useState } from "react";
 
 /**
- * @typedf InterSectionObserverOptions
- * @param {String} rootMargin CSS dimension around the root element top trigger the isIntersecting event
+ * @typedef InterSectionObserverOptions
+ * @field {DOMElement} [root=null] The root element to observe. Keep null to use the browser viewport (window)
+ * @field {String} rootMargin CSS dimension around the root element top trigger the isIntersecting event
+ * @field {Array|Number} [threshhold=[0]]
+ */
+
+/**
+ * @typedef IntersectionObserverEntry
+ * @field {Boolean} isIntersecting
+ * @field {DOMElement} target
+ * @field {Object} boundingClientRect
+ * @field {Number} time
  */
 
 /**
  * Hook to use the IntersectionObserver API on the provided element
- * @param {*} ref Reference to the element to observe
+ * @see https://developer.mozilla.org/fr/docs/Web/API/IntersectionObserver
+ * @param {RefObject} ref Reference to the element to observe
  * @param {InterSectionObserverOptions} options
+ * @return {IntersectionObserverEntry}
  */
 const useIntersectionObserver = (ref, options) => {
+	// @see https://developer.mozilla.org/fr/docs/Web/API/IntersectionObserverEntry
 	const [intersectionObserverEntry, setIntersectionObserverEntry] = useState(null);
 
 	useEffect(() => {
