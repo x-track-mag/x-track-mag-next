c1309ab6c5bfa92a62a3a41e051ddf5b034ab3f8
zipang <christophe.desguez@gmail.com>
2021-07-02 12:31:15 +0200

    Packages upgrade

--- src/components/base/MotionBox.js
+++ src/components/base/MotionBox.js
@@ -1,35 +1,39 @@
-import { forwardRef } from "react";
 import { motion } from "framer-motion";
 import { Box } from "@chakra-ui/react";
 
-/**
- * This will allow us to animate all this typographic elements
- * with framer-motion properties
- * @see https://github.com/chakra-ui/chakra-ui/issues/2538 for the quirks
- * why framer props are not passed to child component
- */
-// const MotionBox = motion.custom(Box);
-const MotionBox = motion.custom(
-	forwardRef(
-		({ whileHover, animate, transition, variants, initial, ...moreProps }, ref) => (
-			<Box ref={ref} {...moreProps} />
-		)
-	)
-);
+// /**
+//  * This will allow us to animate all this typographic elements
+//  * with framer-motion properties
+//  * @see https://github.com/chakra-ui/chakra-ui/issues/2538 for the quirks
+//  * why framer props are not passed to child component
+//  */
+// const MotionBox = motion.custom(
+// 	forwardRef(
+// 		({ whileHover, animate, transition, variants, initial, ...moreProps }, ref) => (
+// 			<Box ref={ref} {...moreProps} />
+// 		)
+// 	)
+// );
+
+// /**
+//  * Build a MotionBox (ie : a mixin between a Chakra Box and a Framer motion div)
+//  * That will always receive default properties (style) and allow to forward `ref` too.
+//  * @param {Object} defaultProps
+//  */
+// export const makeMotionBox = ({ ...defaultProps }) =>
+// 	motion.custom(
+// 		forwardRef(
+// 			(
+// 				{ whileHover, animate, transition, variants, initial, ...moreProps },
+// 				ref
+// 			) => <Box ref={ref} {...defaultProps} {...moreProps} />
+// 		)
+// 	);
 
 /**
- * Build a MotionBox (ie : a mixin between a Chakra Box and a Framer motion div)
- * That will always receive default properties (style) and allow to forward `ref` too.
- * @param {Object} defaultProps
+ * @see https://chakra-ui.com/guides/integrations/with-framer
+ * @see https://stackoverflow.com/questions/66703410/next-js-framermotion-motion-custom-is-not-a-function
+ * Why the trick to manually forward framer property is no longer needed since framer-motion > v3.10
  */
-export const makeMotionBox = ({ ...defaultProps }) =>
-	motion.custom(
-		forwardRef(
-			(
-				{ whileHover, animate, transition, variants, initial, ...moreProps },
-				ref
-			) => <Box ref={ref} {...defaultProps} {...moreProps} />
-		)
-	);
-
+const MotionBox = motion(Box);
 export default MotionBox;
