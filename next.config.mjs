e34cd51a93cc6b6794259e40eb2af862951d40ce
zipang <christophe.desguez@gmail.com>
2025-02-21 11:34:28 +0100

    fix the missing locale

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import { resolve } from "path";
 import __dirname from "./dirname.cjs";
 
 const nextConfig = {
+	i18n: {
+		defaultLocale: "fr",
+		locales: ["fr"],
+	},
 	reactStrictMode: true,
 	images: {
 		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
